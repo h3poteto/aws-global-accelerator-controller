aeeaf506f6acbfe971fa21bff4acfb0e8c47546e
AkiraFukushima <h3.poteto@gmail.com>
2022-03-24 01:16:18 +0900

    Add e2e for ingress

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -2,11 +2,13 @@ package e2e_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/service/globalaccelerator"
 	"github.com/h3poteto/aws-global-accelerator-controller/e2e/pkg/fixtures"
 	cloudaws "github.com/h3poteto/aws-global-accelerator-controller/pkg/cloudprovider/aws"
 
@@ -46,178 +48,152 @@ var _ = BeforeSuite(func() {
 	if namespace == "" {
 		namespace = "default"
 	}
+
+	ctx := context.Background()
+	image := os.Getenv("E2E_MANAGER_IMAGE")
+	Expect(image).ShouldNot(BeEmpty(), "Env var E2E_MANAGER_IMAGE is required")
+	err = fixtures.ApplyClusterRole(ctx, cfg)
+	Expect(err).ShouldNot(HaveOccurred())
+	sa, crb, dep := fixtures.NewManagerManifests(namespace, "aws-global-accelerator-controller", image)
+	_, err = kubeClient.CoreV1().ServiceAccounts(namespace).Create(ctx, sa, metav1.CreateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+	_, err = kubeClient.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+	_, err = kubeClient.AppsV1().Deployments(namespace).Create(ctx, dep, metav1.CreateOptions{})
+	Expect(err).ShouldNot(HaveOccurred())
+
+	DeferCleanup(func() {
+		_ = kubeClient.AppsV1().Deployments(namespace).Delete(ctx, dep.Name, metav1.DeleteOptions{})
+		_ = kubeClient.RbacV1().ClusterRoleBindings().Delete(ctx, crb.Name, metav1.DeleteOptions{})
+		_ = kubeClient.CoreV1().ServiceAccounts(namespace).Delete(ctx, sa.Name, metav1.DeleteOptions{})
+		_ = fixtures.DeleteClusterRole(ctx, cfg)
+	})
+
+	err = wait.Poll(5*time.Second, 2*time.Minute, func() (bool, error) {
+		deploy, err := kubeClient.AppsV1().Deployments(namespace).Get(ctx, dep.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		klog.Infof("Deployment %s/%s is available %d/%d", deploy.Namespace, deploy.Name, deploy.Status.AvailableReplicas, *deploy.Spec.Replicas)
+		if deploy.Status.AvailableReplicas == *deploy.Spec.Replicas && deploy.Status.ReadyReplicas == *deploy.Spec.Replicas {
+			return true, nil
+		}
+		return false, nil
+	})
+	Expect(err).ShouldNot(HaveOccurred(), "Manager pods are not running")
 })
 
 var _ = Describe("E2E", func() {
-	BeforeEach(func() {
-		ctx := context.Background()
-		image := os.Getenv("E2E_MANAGER_IMAGE")
-		Expect(image).ShouldNot(BeEmpty(), "Env var E2E_MANAGER_IMAGE is required")
-		err := fixtures.ApplyClusterRole(ctx, cfg)
-		Expect(err).ShouldNot(HaveOccurred())
-		sa, crb, dep := fixtures.NewManagerManifests(namespace, "aws-global-accelerator-controller", image)
-		_, err = kubeClient.CoreV1().ServiceAccounts(namespace).Create(ctx, sa, metav1.CreateOptions{})
-		Expect(err).ShouldNot(HaveOccurred())
-		_, err = kubeClient.RbacV1().ClusterRoleBindings().Create(ctx, crb, metav1.CreateOptions{})
-		Expect(err).ShouldNot(HaveOccurred())
-		_, err = kubeClient.AppsV1().Deployments(namespace).Create(ctx, dep, metav1.CreateOptions{})
-		Expect(err).ShouldNot(HaveOccurred())
-
-		DeferCleanup(func() {
-			_ = kubeClient.AppsV1().Deployments(namespace).Delete(ctx, dep.Name, metav1.DeleteOptions{})
-			_ = kubeClient.RbacV1().ClusterRoleBindings().Delete(ctx, crb.Name, metav1.DeleteOptions{})
-			_ = kubeClient.CoreV1().ServiceAccounts(namespace).Delete(ctx, sa.Name, metav1.DeleteOptions{})
-			_ = fixtures.DeleteClusterRole(ctx, cfg)
-		})
-
-		err = wait.Poll(5*time.Second, 2*time.Minute, func() (bool, error) {
-			deploy, err := kubeClient.AppsV1().Deployments(namespace).Get(ctx, dep.Name, metav1.GetOptions{})
-			if err != nil {
-				return false, err
-			}
-			klog.Infof("Deployment %s/%s is available %d/%d", deploy.Namespace, deploy.Name, deploy.Status.AvailableReplicas, *deploy.Spec.Replicas)
-			if deploy.Status.AvailableReplicas == *deploy.Spec.Replicas && deploy.Status.ReadyReplicas == *deploy.Spec.Replicas {
-				return true, nil
-			}
-			return false, nil
-		})
-		Expect(err).ShouldNot(HaveOccurred(), "Manager pods are not running")
-
-	})
-	Describe("Service type Load Balancer", func() {
-		It("Global Accelerator and Route53 should be created", func() {
-			ctx := context.Background()
-			svc := fixtures.NewNLBService(namespace, "e2e-test", hostname)
-			_, err := kubeClient.CoreV1().Services(namespace).Create(ctx, svc, metav1.CreateOptions{})
-			Expect(err).ShouldNot(HaveOccurred())
-
-			DeferCleanup(func() {
-				kubeClient.CoreV1().Services(svc.Namespace).Delete(ctx, svc.Name, metav1.DeleteOptions{})
-				// TODO: wait until cleanup
-			})
-
-			By("Wait until LoadBalancer is created", func() {
-				err = wait.PollImmediate(10*time.Second, 5*time.Minute, func() (bool, error) {
-					currentService, err := kubeClient.CoreV1().Services(namespace).Get(ctx, svc.Name, metav1.GetOptions{})
-					if err != nil {
-						return false, err
-					}
-					if len(currentService.Status.LoadBalancer.Ingress) > 0 {
-						return true, nil
-					}
-					klog.Infof("%s/%s does not have loadBalancer yet", currentService.Namespace, currentService.Name)
-					return false, nil
-				})
-				Expect(err).ShouldNot(HaveOccurred())
-			})
-
-			svc, err = kubeClient.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, metav1.GetOptions{})
-			Expect(err).ShouldNot(HaveOccurred())
-
-			lbName, region, err := cloudaws.GetLBNameFromHostname(svc.Status.LoadBalancer.Ingress[0].Hostname)
-			Expect(err).ShouldNot(HaveOccurred())
-			cloud := cloudaws.NewAWS(region)
-
-			By("Wait until Global Accelerator is created", func() {
-				lb, err := cloud.GetLoadBalancer(ctx, lbName)
+	Describe("Global Accelerator and Route53", func() {
+		Context("Service type Load Balancer", func() {
+			It("Resources should be created", func() {
+				ctx := context.Background()
+				svc := fixtures.NewNLBService(namespace, "e2e-test", hostname)
+				svc, err := kubeClient.CoreV1().Services(namespace).Create(ctx, svc, metav1.CreateOptions{})
 				Expect(err).ShouldNot(HaveOccurred())
 
-				err = wait.Poll(10*time.Second, 5*time.Minute, func() (bool, error) {
-					accelerators, err := cloud.ListGlobalAcceletaroByResource(ctx, "service", svc.Namespace, svc.Name)
-					if err != nil {
-						return false, err
-					}
-					if len(accelerators) == 0 {
-						klog.Infof("There is no accelerator related Service %s/%s", svc.Namespace, svc.Name)
-						return false, nil
-					}
-					for _, accelerator := range accelerators {
-						listener, err := cloud.GetListener(ctx, *accelerator.AcceleratorArn)
-						if err != nil {
-							return false, err
-						}
-						endpoint, err := cloud.GetEndpointGroup(ctx, *listener.ListenerArn)
-						if err != nil {
-							return false, err
-						}
-						for _, d := range endpoint.EndpointDescriptions {
-							if *d.EndpointId == *lb.LoadBalancerArn {
-								return true, nil
-							}
-						}
-					}
-					klog.Infof("There is no endpoint group related Service %s/%s", svc.Namespace, svc.Name)
-					return false, nil
-				})
-				Expect(err).ShouldNot(HaveOccurred())
-			})
+				hostnames := strings.Split(hostname, ",")
 
-			hostnames := strings.Split(hostname, ",")
-
-			By("Wait until Route53 record is created", func() {
-				accelerators, err := cloud.ListGlobalAcceleratorByHostname(ctx, svc.Status.LoadBalancer.Ingress[0].Hostname, "service", svc.Namespace, svc.Name)
-				Expect(err).ShouldNot(HaveOccurred())
-				acceleratorHostname := *accelerators[0].DnsName
-
-				for _, h := range hostnames {
-					hostedZone, err := cloud.GetHostedZone(ctx, h)
-					Expect(err).ShouldNot(HaveOccurred())
-
+				By("Wait until LoadBalancer is created", func() {
 					err = wait.PollImmediate(10*time.Second, 5*time.Minute, func() (bool, error) {
-						records, err := cloud.FindOwneredARecordSets(ctx, hostedZone, cloudaws.Route53OwnerValue("service", svc.Namespace, svc.Name))
+						currentService, err := kubeClient.CoreV1().Services(namespace).Get(ctx, svc.Name, metav1.GetOptions{})
 						if err != nil {
 							return false, err
 						}
-						if len(records) == 0 {
-							klog.Infof("There is no route53 record related Service %s/%s", svc.Namespace, svc.Name)
-							return false, nil
-						}
-						for _, record := range records {
-							if record.AliasTarget != nil && *record.AliasTarget.DNSName == acceleratorHostname+"." {
-								return true, nil
-							}
-						}
-						klog.Infof("There is no route53 record related Global Accelerator: %s", acceleratorHostname)
-						return false, nil
-					})
-					Expect(err).ShouldNot(HaveOccurred())
-				}
-			})
-
-			By("Remove resources", func() {
-				err := kubeClient.CoreV1().Services(svc.Namespace).Delete(ctx, svc.Name, metav1.DeleteOptions{})
-				Expect(err).ShouldNot(HaveOccurred())
-
-				for _, h := range hostnames {
-					hostedZone, err := cloud.GetHostedZone(ctx, h)
-					Expect(err).ShouldNot(HaveOccurred())
-					err = wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
-						records, err := cloud.FindOwneredARecordSets(ctx, hostedZone, cloudaws.Route53OwnerValue("service", svc.Namespace, svc.Name))
-						if err != nil {
-							return false, err
-						}
-						if len(records) == 0 {
+						if len(currentService.Status.LoadBalancer.Ingress) > 0 {
 							return true, nil
 						}
-						klog.Info("There are some Route53 records")
+						klog.Infof("%s/%s does not have loadBalancer yet", currentService.Namespace, currentService.Name)
 						return false, nil
 					})
 					Expect(err).ShouldNot(HaveOccurred())
-				}
-
-				err = wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
-					accelerators, err := cloud.ListGlobalAcceletaroByResource(ctx, "service", svc.Namespace, svc.Name)
-					if err != nil {
-						return false, err
-					}
-					if len(accelerators) == 0 {
-						return true, nil
-					}
-					klog.Info("There are some Global Accelerators")
-					return false, nil
 				})
+
+				svc, err = kubeClient.CoreV1().Services(svc.Namespace).Get(ctx, svc.Name, metav1.GetOptions{})
 				Expect(err).ShouldNot(HaveOccurred())
 
+				lbName, region, err := cloudaws.GetLBNameFromHostname(svc.Status.LoadBalancer.Ingress[0].Hostname)
+				Expect(err).ShouldNot(HaveOccurred())
+				cloud := cloudaws.NewAWS(region)
+
+				DeferCleanup(func() error {
+					kubeClient.CoreV1().Services(svc.Namespace).Delete(ctx, svc.Name, metav1.DeleteOptions{})
+					err = waitUntilCleanup(cloud, hostnames, "service", svc)
+					return err
+				})
+
+				By("Wait until Global Accelerator is created", func() {
+					err = waitUntilGlobalAccelerator(cloud, lbName, "service", svc)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+
+				By("Wait until Route53 record is created", func() {
+					err = waitUntilRoute53(cloud, hostnames, svc.Status.LoadBalancer.Ingress[0].Hostname, "service", svc)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+
+				By("Remove resources", func() {
+					err := kubeClient.CoreV1().Services(svc.Namespace).Delete(ctx, svc.Name, metav1.DeleteOptions{})
+					Expect(err).ShouldNot(HaveOccurred())
+					err = waitUntilCleanup(cloud, hostnames, "service", svc)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+			})
+		})
+
+		Context("Ingress Load Balancer", func() {
+			It("Resources should be created", func() {
+				ctx := context.Background()
+				ingress := fixtures.NewALBIngress(namespace, "e2e-test", hostname)
+				ingress, err := kubeClient.NetworkingV1().Ingresses(namespace).Create(ctx, ingress, metav1.CreateOptions{})
+				Expect(err).ShouldNot(HaveOccurred())
+
+				hostnames := strings.Split(hostname, ",")
+
+				By("Wait until LoadBalancer is created", func() {
+					err = wait.PollImmediate(10*time.Second, 5*time.Minute, func() (bool, error) {
+						currentIngress, err := kubeClient.NetworkingV1().Ingresses(namespace).Get(ctx, ingress.Name, metav1.GetOptions{})
+						if err != nil {
+							return false, err
+						}
+						if len(currentIngress.Status.LoadBalancer.Ingress) > 0 {
+							return true, nil
+						}
+						klog.Infof("%s/%s does not have loadBalancer yet", currentIngress.Namespace, currentIngress.Name)
+						return false, nil
+					})
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+
+				ingress, err = kubeClient.NetworkingV1().Ingresses(ingress.Namespace).Get(ctx, ingress.Name, metav1.GetOptions{})
+				Expect(err).ShouldNot(HaveOccurred())
+
+				lbName, region, err := cloudaws.GetLBNameFromHostname(ingress.Status.LoadBalancer.Ingress[0].Hostname)
+				Expect(err).ShouldNot(HaveOccurred())
+				cloud := cloudaws.NewAWS(region)
+
+				DeferCleanup(func() error {
+					kubeClient.NetworkingV1().Ingresses(ingress.Namespace).Delete(ctx, ingress.Name, metav1.DeleteOptions{})
+					err = waitUntilCleanup(cloud, hostnames, "ingress", ingress)
+					return err
+				})
+
+				By("Wait until Global Accelerator is created", func() {
+					err = waitUntilGlobalAccelerator(cloud, lbName, "ingress", ingress)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+
+				By("Wait until Route53 record is created", func() {
+					err = waitUntilRoute53(cloud, hostnames, ingress.Status.LoadBalancer.Ingress[0].Hostname, "ingress", ingress)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+
+				By("Remove resources", func() {
+					err := kubeClient.NetworkingV1().Ingresses(ingress.Namespace).Delete(ctx, ingress.Name, metav1.DeleteOptions{})
+					Expect(err).ShouldNot(HaveOccurred())
+					err = waitUntilCleanup(cloud, hostnames, "ingress", ingress)
+					Expect(err).ShouldNot(HaveOccurred())
+				})
 			})
 		})
 	})
@@ -255,4 +231,132 @@ func nodeIsReady(node *corev1.Node) bool {
 		}
 	}
 	return false
+}
+
+func waitUntilGlobalAccelerator(cloud *cloudaws.AWS, lbName, resource string, obj metav1.Object) error {
+	ctx := context.Background()
+	lb, err := cloud.GetLoadBalancer(ctx, lbName)
+	if err != nil {
+		return err
+	}
+
+	err = wait.Poll(10*time.Second, 10*time.Minute, func() (bool, error) {
+		accelerators, err := cloud.ListGlobalAcceleratorByResource(ctx, resource, obj.GetNamespace(), obj.GetName())
+		if err != nil {
+			return false, err
+		}
+		if len(accelerators) == 0 {
+			klog.Infof("There is no accelerator related %s %s/%s", resource, obj.GetNamespace(), obj.GetName())
+			return false, nil
+		}
+		for _, accelerator := range accelerators {
+			listener, err := cloud.GetListener(ctx, *accelerator.AcceleratorArn)
+			var listenerNotFoundErr *globalaccelerator.ListenerNotFoundException
+			if errors.As(err, &listenerNotFoundErr) {
+				klog.Info(err.Error())
+				return false, nil
+			}
+			if err != nil {
+				return false, err
+			}
+			endpoint, err := cloud.GetEndpointGroup(ctx, *listener.ListenerArn)
+			var endpointNotFoundErr *globalaccelerator.EndpointGroupNotFoundException
+			if errors.As(err, &endpointNotFoundErr) {
+				klog.Info(err.Error())
+				return false, nil
+			}
+			if err != nil {
+				return false, err
+			}
+			for _, d := range endpoint.EndpointDescriptions {
+				if *d.EndpointId == *lb.LoadBalancerArn {
+					return true, nil
+				}
+			}
+		}
+		klog.Infof("There is no endpoint group related %s %s/%s", resource, obj.GetNamespace(), obj.GetName())
+		return false, nil
+	})
+	return err
+}
+
+func waitUntilRoute53(cloud *cloudaws.AWS, hostnames []string, lbHostname, resource string, obj metav1.Object) error {
+	ctx := context.Background()
+	accelerators, err := cloud.ListGlobalAcceleratorByHostname(ctx, lbHostname, resource, obj.GetNamespace(), obj.GetName())
+	if err != nil {
+		return err
+	}
+	acceleratorHostname := *accelerators[0].DnsName
+
+	for _, h := range hostnames {
+		hostedZone, err := cloud.GetHostedZone(ctx, h)
+		Expect(err).ShouldNot(HaveOccurred())
+
+		err = wait.PollImmediate(10*time.Second, 5*time.Minute, func() (bool, error) {
+			records, err := cloud.FindOwneredARecordSets(ctx, hostedZone, cloudaws.Route53OwnerValue(resource, obj.GetNamespace(), obj.GetName()))
+			if err != nil {
+				return false, err
+			}
+			if len(records) == 0 {
+				klog.Infof("There is no route53 record related %s %s/%s", resource, obj.GetNamespace(), obj.GetName())
+				return false, nil
+			}
+			for _, record := range records {
+				if record.AliasTarget != nil && *record.AliasTarget.DNSName == acceleratorHostname+"." {
+					return true, nil
+				}
+			}
+			klog.Infof("There is no route53 record related Global Accelerator: %s", acceleratorHostname)
+			return false, nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func waitUntilCleanup(cloud *cloudaws.AWS, hostnames []string, resource string, obj metav1.Object) error {
+	if cloud == nil {
+		return nil
+	}
+	if obj == nil {
+		return nil
+	}
+	ctx := context.Background()
+	for _, h := range hostnames {
+		hostedZone, err := cloud.GetHostedZone(ctx, h)
+		if err != nil {
+			return err
+		}
+		err = wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
+			records, err := cloud.FindOwneredARecordSets(ctx, hostedZone, cloudaws.Route53OwnerValue(resource, obj.GetNamespace(), obj.GetName()))
+			if err != nil {
+				return false, err
+			}
+			if len(records) == 0 {
+				klog.Info("All Route53 records are deleted")
+				return true, nil
+			}
+			klog.Info("There are some Route53 records")
+			return false, nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	err := wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
+		accelerators, err := cloud.ListGlobalAcceleratorByResource(ctx, resource, obj.GetNamespace(), obj.GetName())
+		if err != nil {
+			return false, err
+		}
+		if len(accelerators) == 0 {
+			klog.Info("All Global Accelerators are deleted")
+			return true, nil
+		}
+		klog.Info("There are some Global Accelerators")
+		return false, nil
+	})
+	return err
 }
